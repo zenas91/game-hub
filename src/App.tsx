da60436c119c3550bb2a9c92cb844100912b4338
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-03-21 16:36:41 +0100

    Setup Responsive Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorPalette="blue">Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold" hideBelow="lg">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
