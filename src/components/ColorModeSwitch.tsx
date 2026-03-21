686c673cbdc7fd37295c47e004c1fcbbd1698fe0
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-03-21 17:46:32 +0100

    Implemented ColorMode and ColorModeSwitch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,22 @@
+import { HStack, Switch, Text } from "@chakra-ui/react";
+import { useColorMode } from "@/components/ui/color-mode";
+import React from "react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <HStack>
+      <Switch.Root
+        colorPalette="green"
+        checked={colorMode === "dark"}
+        onCheckedChange={toggleColorMode}
+      >
+        <Switch.HiddenInput />
+        <Switch.Control />
+        <Switch.Label>Dark Mode</Switch.Label>
+      </Switch.Root>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
