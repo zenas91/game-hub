45ad13d7930b182ea357eda0bb22602ffd8270a3
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-03-22 15:27:52 +0100

    Fetched Games using Custom Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Button, Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -12,11 +13,11 @@ function App() {
       <GridItem area="nav">
         <NavBar />
       </GridItem>
-      <GridItem area="aside" bg="gold" hideBelow="lg">
+      <GridItem area="aside" hideBelow="lg">
         Aside
       </GridItem>
-      <GridItem area="main" bg="dodgerblue">
-        Main
+      <GridItem area="main">
+        <GameGrid />
       </GridItem>
     </Grid>
   );
---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,20 @@
+import useGames from "@/hooks/useGames";
+import apiClient from "@/services/api-client";
+import { Text } from "@chakra-ui/react";
+
+const GameGrid = () => {
+  const { games, error } = useGames();
+
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GameGrid;
