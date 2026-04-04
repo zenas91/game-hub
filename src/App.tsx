7c2ab694fce03b5cdd82ea48f9a41c84e092d06f
Chukwuebuka Uchechukwu <zenaszone@gmail.com>
2026-04-04 17:44:49 +0200

    Resolved Import Error

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
-import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
+import { Genre } from "./services/genre-service";
 
 export interface GameQuery {
   genre: Genre | null;
